d7ea31fd3359cb8fff4b27868557b2566ee2b44d
Saulo Januzzi <sjanuzzi@gmail.com>
2019-09-18 10:08:15 -0300

    incluindo o campo observacoes na view

--- atracoes/api/serializers.py
+++ atracoes/api/serializers.py
@@ -4,4 +4,4 @@ from atracoes.models import Atracao
 class AtracoesSerializer(ModelSerializer):
 	class Meta:
 		model = Atracao
-		fields = ('id', 'nome','descricao', 'horario_func', 'idade_minima', 'foto')
+		fields = ('id', 'nome','descricao', 'horario_func', 'idade_minima', 'foto', 'observacoes')
